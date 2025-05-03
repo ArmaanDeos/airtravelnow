aa77f9ace42718946ea1e08b74a18c734c7410c3
Armaan Ahmad <99592377+ArmaanDeos@users.noreply.github.com>
2025-05-03 14:26:39 +0530

    Update duffel.js

--- server/src/config/duffel.js
+++ server/src/config/duffel.js
@@ -1,9 +1,6 @@
 import { Duffel } from "@duffel/api";
 import dotenv from "dotenv";
 dotenv.config();
-
-// ✅ Add this line to test the environment variable
-// console.log("Duffel API Key:", process.env.DUFFEL_API_KEY);
 const duffel = new Duffel({
   token: process.env.DUFFEL_API_KEY,
 });
